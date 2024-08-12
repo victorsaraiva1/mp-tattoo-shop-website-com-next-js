2f0bbcdbc015a7f850b7dc01fba4e26bb1ab36e5
Victor Saraiva <vf.ramos7@gmail.com>
2024-08-12 17:11:32 -0300

    Finishing page 'about' and 'contact'. Iniciating process to make it responsive.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,29 @@
+import Image from 'next/image';
+import Portrait from '@/app/_assets/Portrait.png';
+import { islandMoments } from '../_lib/fonts';
+
 export default function About() {
   return (
-    <h1 className="text-cyan-300">
-      Página de Sobre Mim
-    </h1>
+    <main className="relative flex-1">
+
+      <h1 className='mt-12 text-3xl font-black'>
+        Prazer, sou <span className='text-yellow-300'>Sando Luz</span>
+      </h1>
+
+      <section className="font-light text-zinc-400 max-w-3xl space-y-4 mt-8">
+        <p>Olá! Sou Sandro Luz, apaixonado pela arte que transcende o tempo e se imprime na pele. Com mais de 10 anos de experiência no mundo da tatuagem, dedico-me a transformar visões e emoções em arte corporal que expressa quem você é e o que você ama.</p>
+        <p>Cada tatuagem em meu estúdio é um trabalho de colaboração; ouço atentamente os desejos e as histórias de meus clientes para que cada peça seja única e pessoal. Especializo-me em vários estilos, desde o delicado traço fino até composições mais robustas e coloridas, garantindo que cada design não só atenda, mas supere as expectativas.</p>
+        <p>Além da dedicação à arte, a segurança e o conforto são prioridades no meu estúdio. Utilizo apenas equipamentos esterilizados e de alta qualidade, e sou rigoroso quanto aos protocolos de higiene e segurança, garantindo uma experiência segura e confortável para todos.</p>
+        <p>Visite meu estúdio e veja como a arte pode se manifestar em sua pele. Estou aqui para ajudá-lo a contar sua história de uma maneira única e eterna. Agende sua consulta e vamos juntos criar algo extraordinário!</p>
+      </section>
+
+      <h1 className={`mt-8 text-4xl ${islandMoments.className}`}>Sandro Luz</h1>
+
+      <Image
+        className="absolute bottom-0 right-0 w-80"
+        src={Portrait}
+        alt="Sandro Luz Portrait"
+      />
+    </main>
   );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import ContactImage from '@/app/_assets/Contact.png';
+import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
+
 export default function Contact() {
   return (
-    <h1 className="text-cyan-300">
-      Página de Contatos
-    </h1>
+    <main className="flex gap-10 justify-between">
+      <section>
+        <h1 className="font-black text-3xl mt-12">Entre em <span className="text-yellow-400">contato</span></h1>
+
+        <p className="text-sm font-extralight text-zinc-400 mt-12">
+          Clique no botão abaixo e envie uma mensagem. <br />
+          Vamos conversar!
+        </p>
+
+        <Link target="_blank" href={`https://wa.me/5511999999999?text=Ol%C3%A1%20Sandro!%20Desejo%20fazer%20um%20or%C3%A7amento.`}>
+          <button className="mt-12 flex items-center gap-2 p-4 bg-zinc-700 border-b-4 border-zinc-800 rounded-md">
+            <FaWhatsapp className="w-8 h-8" />
+
+            <div>
+              <p className="text-sm">Enviar mensagem</p>
+              <p className="items-center text-xs text-zinc-400 font-extralight">+55 11 99999-9999</p>
+            </div>
+          </button>
+        </Link>
+      </section>
+
+      <Image
+        src={ContactImage}
+        alt="Tattoo Artist Working"
+        width={700}
+      />
+    </main>
   );
 }
